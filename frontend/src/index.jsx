9492f1a4691563140a1998dde8fe192e6a25f6bd
Alongkorn Anuwatprakit <alongkorn29380@gmail.com>
2026-06-17 12:15:44 +0700

    initial: project structure + env setup

---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+import ReactDom from 'react-dom/client'
+import Experience from './Experience.jsx'
+
+const root = ReactDom.createRoot(document.querySelector('#root'))
+
+root.render(<Experience />)
